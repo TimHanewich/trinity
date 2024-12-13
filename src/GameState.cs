e7da0db046b240b9911b13eaf3acef59cd64b66e
Tim Hanewich <tahanewich@live.com>
2024-12-13 18:19:14 -0500

    Consolidated FEN parsing to one function

--- src/GameState.cs
+++ src/GameState.cs
@@ -26,91 +26,86 @@ namespace Chess3
             //Strip out board squares portion
             string[] parts = FEN.Split(" ");
 
-            //Board rep
+            //Board rep v2
             string BoardRepresentation = parts[0];
             string[] ranks = BoardRepresentation.Split("/");
-            ToReturn.LoadRowFEN(ranks[7], 7);
-            ToReturn.LoadRowFEN(ranks[6], 6);
-            ToReturn.LoadRowFEN(ranks[5], 5);
-            ToReturn.LoadRowFEN(ranks[4], 4);
-            ToReturn.LoadRowFEN(ranks[3], 3);
-            ToReturn.LoadRowFEN(ranks[2], 2);
-            ToReturn.LoadRowFEN(ranks[1], 1);
-            ToReturn.LoadRowFEN(ranks[0], 0);
+            int onRank = 7;
+            foreach (string rank in ranks)
+            {
+                //Replace numbers with actual single space representations (makes it easier to handle each)
+                string row_fen = rank; //take a "copy of it" that we can manipulate
+                row_fen = row_fen.Replace("8", "11111111");
+                row_fen = row_fen.Replace("7", "1111111");
+                row_fen = row_fen.Replace("6", "111111");
+                row_fen = row_fen.Replace("5", "11111");
+                row_fen = row_fen.Replace("4", "1111");
+                row_fen = row_fen.Replace("3", "111");
+                row_fen = row_fen.Replace("2", "11");
+
+                int onFile = 0;
+                foreach (char c in row_fen)
+                {
+                    if (char.IsNumber(c) == false)
+                    {
+                        Square ThisPosition = (Square)((8*onRank) + onFile);
+                        if (c == 'P')
+                        {
+                            ToReturn.WhitePawns = ToReturn.WhitePawns.SetBit(ThisPosition, true);
+                        }
+                        else if (c == 'N')
+                        {
+                            ToReturn.WhiteKnights = ToReturn.WhiteKnights.SetBit(ThisPosition, true);
+                        }
+                        else if (c == 'B')
+                        {
+                            ToReturn.WhiteBishops = ToReturn.WhiteBishops.SetBit(ThisPosition, true);
+                        }
+                        else if (c == 'R')
+                        {
+                            ToReturn.WhiteRooks = ToReturn.WhiteRooks.SetBit(ThisPosition, true);
+                        }
+                        else if (c == 'Q')
+                        {
+                            ToReturn.WhiteQueens = ToReturn.WhiteQueens.SetBit(ThisPosition, true);
+                        }
+                        else if (c == 'K')
+                        {
+                            ToReturn.WhiteKings = ToReturn.WhiteKings.SetBit(ThisPosition, true);
+                        }
+                        else if (c == 'p')
+                        {
+                            ToReturn.BlackPawns = ToReturn.BlackPawns.SetBit(ThisPosition, true);
+                        }
+                        else if (c == 'n')
+                        {
+                            ToReturn.BlackKnights = ToReturn.BlackKnights.SetBit(ThisPosition, true);
+                        }
+                        else if (c == 'b')
+                        {
+                            ToReturn.BlackBishops = ToReturn.BlackBishops.SetBit(ThisPosition, true);
+                        }
+                        else if (c == 'r')
+                        {
+                            ToReturn.BlackRooks = ToReturn.BlackRooks.SetBit(ThisPosition, true);
+                        }
+                        else if (c == 'q')
+                        {
+                            ToReturn.BlackQueens = ToReturn.BlackQueens.SetBit(ThisPosition, true);
+                        }
+                        else if (c == 'k')
+                        {
+                            ToReturn.BlackKings = ToReturn.BlackKings.SetBit(ThisPosition, true);
+                        }
+                    }
+                    onFile = onFile + 1; //increment the file we're on
+                }  
+
+                //Decrement the rank we are on (FEN notation starts @ rank 8 and goes down from there)
+                onRank = onRank - 1;   
+            }
+            
 
             return ToReturn;
-        }
-
-        //Loads a single "row" (rank) of the FEN's board representation into the appropriate bitboards
-        //parameter "rank" should be 0-based... so rank 1 would be 0, rank 8 would be 7
-        private void LoadRowFEN(string row_fen, int rank)
-        {
-            //Replace numbers with actual single space representations (makes it easier to handle each)
-            row_fen = row_fen.Replace("8", "11111111");
-            row_fen = row_fen.Replace("7", "1111111");
-            row_fen = row_fen.Replace("6", "111111");
-            row_fen = row_fen.Replace("5", "11111");
-            row_fen = row_fen.Replace("4", "1111");
-            row_fen = row_fen.Replace("3", "111");
-            row_fen = row_fen.Replace("2", "11");
-
-            int onFile = 0;
-            foreach (char c in row_fen)
-            {
-                if (char.IsNumber(c) == false)
-                {
-                    Square ThisPosition = (Square)((8*rank) + onFile);
-                    if (c == 'P')
-                    {
-                        WhitePawns = WhitePawns.SetBit(ThisPosition, true);
-                    }
-                    else if (c == 'N')
-                    {
-                        WhiteKnights = WhiteKnights.SetBit(ThisPosition, true);
-                    }
-                    else if (c == 'B')
-                    {
-                        WhiteBishops = WhiteBishops.SetBit(ThisPosition, true);
-                    }
-                    else if (c == 'R')
-                    {
-                        WhiteRooks = WhiteRooks.SetBit(ThisPosition, true);
-                    }
-                    else if (c == 'Q')
-                    {
-                        WhiteQueens = WhiteQueens.SetBit(ThisPosition, true);
-                    }
-                    else if (c == 'K')
-                    {
-                        WhiteKings = WhiteKings.SetBit(ThisPosition, true);
-                    }
-                    else if (c == 'p')
-                    {
-                        BlackPawns = BlackPawns.SetBit(ThisPosition, true);
-                    }
-                    else if (c == 'n')
-                    {
-                        BlackKnights = BlackKnights.SetBit(ThisPosition, true);
-                    }
-                    else if (c == 'b')
-                    {
-                        BlackBishops = BlackBishops.SetBit(ThisPosition, true);
-                    }
-                    else if (c == 'r')
-                    {
-                        BlackRooks = BlackRooks.SetBit(ThisPosition, true);
-                    }
-                    else if (c == 'q')
-                    {
-                        BlackQueens = BlackQueens.SetBit(ThisPosition, true);
-                    }
-                    else if (c == 'k')
-                    {
-                        BlackKings = BlackKings.SetBit(ThisPosition, true);
-                    }
-                }
-                onFile = onFile + 1; //increment the file we're on
-            }     
         }
     }
 }
