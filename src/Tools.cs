e528a65333a808d0dbc1e8d8197c20aa553f0b28
Tim Hanewich <tahanewich@live.com>
2024-12-13 16:36:38 -0500

    Changed terminology, use enum

--- src/Tools.cs
+++ src/Tools.cs
@@ -22,26 +22,26 @@ namespace Chess3
             }
         }
 
-        public static ulong SetBit(ulong original, int position, bool value)
+        public static ulong SetBit(ulong board, Square position, bool value)
         {
             if (value)
             {
-                ulong mask = 1UL << position;
-                return original | mask; //OR operator.
+                ulong mask = 1UL << Convert.ToInt32(position);
+                return board | mask; //OR operator.
             }
             else
             {
-                ulong mask = 1UL << position; //Set up a mask with 1 at the target position
+                ulong mask = 1UL << Convert.ToInt32(position); //Set up a mask with 1 at the target position
                 mask = ~mask; //invert the mask to get a mask with 0 at the position and 1s everywhere else
-                return original & mask; //use the AND operator to clear out the bit (if there is one) at the target position (the AND operator does NOT go to 1 if both in the mask are 0... it defaults to 0 in that case)
+                return board & mask; //use the AND operator to clear out the bit (if there is one) at the target position (the AND operator does NOT go to 1 if both in the mask are 0... it defaults to 0 in that case)
             }
         }
 
         //Checks if a specific bit at a specific position is 1
-        public static bool BitOccupied(ulong value, int position)
+        public static bool BitOccupied(ulong board, Square position)
         {
-            ulong mask = 1UL << position;
-            ulong AfterAnd = value & mask; //An AND operation. This value will have a 1 in it if the bit of position indeed ALSO had a 1 in it. Thus, we can determine YES, this bit was occupied if the value is > 0 and NO it was not occupied if it was 0.
+            ulong mask = 1UL << Convert.ToInt32(position);
+            ulong AfterAnd = board & mask; //An AND operation. This value will have a 1 in it if the bit of position indeed ALSO had a 1 in it. Thus, we can determine YES, this bit was occupied if the value is > 0 and NO it was not occupied if it was 0.
             return AfterAnd > 0; //If it is 0, it was not occupied. If it was some value > 0, that specific bit was occupied.
         }
 
