b3100e349d8e628dac084fb942df02641d7c3999
Tim Hanewich <tahanewich@live.com>
2024-12-15 05:58:53 -0500

    Testing

--- src/Program.cs
+++ src/Program.cs
@@ -10,14 +10,18 @@ namespace Chess3
     {
         public static void Main(string[] args)
         {
-            GameState gs = GameState.Load("PPPPPPPP/8/8/Q7/7Q/8/8/8");
+
+            GameState gs = GameState.Load("8/8/2Q5/8/8/8/8/8 w - - 0 1");
             gs.Print();
-            Console.WriteLine("0000000000000000");
+
             
             GameState[] nextstates = gs.PossibleNextStates();
+            Console.WriteLine(nextstates.Length.ToString() + " potential next states");
+            Console.ReadLine();
             foreach (GameState ngs in nextstates)
             {
                 ngs.Print();
+                Console.ReadLine();
             }
         }
 
