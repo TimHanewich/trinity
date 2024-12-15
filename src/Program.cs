f598b27561b7be60231949b19dbda9e4b5be93ef
Tim Hanewich <tahanewich@live.com>
2024-12-14 22:26:21 -0500

    Linear move V2

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlTypes;
 using System.IO.Compression;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -9,14 +10,12 @@ namespace Chess3
     {
         public static void Main(string[] args)
         {
-            GameState gs = GameState.Load("pPPPPPPP/8/8/Q7/7Q/8/8/8");
+            GameState gs = GameState.Load("PPPPPPPP/8/8/Q7/7Q/8/8/8");
             gs.Print();
             
-            GameState[] upMoves = LinearMoves(gs, Square.A5, 0);
-
-            Console.WriteLine("------");
-
-            foreach (GameState ngs in upMoves)
+            GameState [] moves = LinearMovesV2(gs, Square.A5, new int[]{8});
+            Console.WriteLine("--------");
+            foreach (GameState ngs in moves)
             {
                 ngs.Print();
             }
@@ -112,6 +111,64 @@ namespace Chess3
                     GameState pgs = state; // "copy" the game
                     pgs.MovePiece(origin, PotentialTarget); //Move the piece, also capturing.
                     ToReturn.Add(pgs);
+                }
+            }
+
+            return ToReturn.ToArray();
+        }
+
+        public static GameState[] LinearMovesV2(GameState state, Square origin, int[] shifts)
+        {
+
+            //Determine positions of "friendly" pieces and positions of "enemy" pieces
+            ulong white = state.White; //Get bitboard of ALL white pieces
+            ulong black = state.Black; //Get bitboard of ALL black pieces
+            ulong friendlies; //"label" we will asign to either white/black
+            ulong enemies; //"label" we will asign to either white/black
+            if (white.SquareOccupied(origin)) //If the piece we are generating moves for is white, white are friendlies.
+            {
+                friendlies = white;
+                enemies = black;
+            }
+            else if (black.SquareOccupied(origin)) //If the piece we are generating moves for is black, black are friendlies.
+            {
+                friendlies = black;
+                enemies = white;
+            }
+            else
+            {
+                throw new Exception("Unable to generate linear moves for piece on square " + origin.ToString() + " because a piece wasn't detected on that square (of either color)!");
+            }
+
+            //Calculate potential next states
+            List<GameState> ToReturn = new List<GameState>();
+            foreach (int shift in shifts)
+            {
+                Square NewPosition = origin;
+                while (true)
+                {
+                    NewPosition = (Square)(NewPosition + shift); //move the potential target by one to simulate the movement of the piece, by just one step.
+                    if (Convert.ToInt32(NewPosition) > 63 || Convert.ToInt32(NewPosition) < 0) //We are "out of bounds", not on the chess board, so break the loop.
+                    {
+                        break; 
+                    }
+                    else if (friendlies.SquareOccupied(NewPosition)) //If there is a "friendly" occupying this square, we can't move to it. So just break!
+                    {
+                        break; 
+                    }
+                    else if (enemies.SquareOccupied(NewPosition)) //There is an enemy piece in this position. 
+                    {
+                        GameState pgs = state; // "copy" the game
+                        pgs.MovePiece(origin, NewPosition); //Move the piece, also capturing.
+                        ToReturn.Add(pgs); //Add it to the list
+                        break; //We can capture this piece but cannot continue to move "past" this piece, so break.
+                    }
+                    else //It is an empty space! So add it! And then continue!
+                    {
+                        GameState pgs = state; // "copy" the game
+                        pgs.MovePiece(origin, NewPosition); //Move the piece, also capturing.
+                        ToReturn.Add(pgs); //Add it to the list
+                    }
                 }
             }
 
