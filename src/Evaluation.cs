0e7951500a6b2a69c25f2626e314192fa7a863b9
Tim Hanewich <tahanewich@live.com>
2024-12-15 11:56:59 -0500

    Added pawn ranking eval function

--- src/Evaluation.cs
+++ src/Evaluation.cs
@@ -106,7 +106,7 @@ namespace Chess3
         #region "internal tools"
 
         //Get additional scoring based on a particular colors center of board positioning
-        public static float CenterOfBoardEvaluation(ulong all_color_pieces)
+        private static float CenterOfBoardEvaluation(ulong all_color_pieces)
         {
             float[] CenterOfBoardSquareWeights = new float[]{0f,0f,0f,0f,0f,0f,0f,0f, 0f,0.05f,0.05f,0.05f,0.05f,0.05f,0.05f,0f, 0f,0.05f,0.1f,0.1f,0.1f,0.1f,0.05f,0f, 0f,0.05f,0.1f,0.15f,0.15f,0.1f,0.05f,0f, 0f,0.05f,0.1f,0.15f,0.15f,0.1f,0.05f,0f, 0f,0.05f,0.1f,0.1f,0.1f,0.1f,0.05f,0f, 0f,0.05f,0.05f,0.05f,0.05f,0.05f,0.05f,0f, 0f,0f,0f,0f,0f,0f,0f,0f}; //Weighted eval scores to boost pieces on certain squares, rewarding more for the center squares. In the order of A1, B1, C1, D1 etc... (same as Square enum) 
             float ToReturn = 0.0f;
@@ -124,6 +124,93 @@ namespace Chess3
         public static float CenterOfBoardEvaluation(this GameState gs)
         {
             return CenterOfBoardEvaluation(gs.White) - CenterOfBoardEvaluation(gs.Black);
+        }
+
+        private static float PawnRankingEvaluation(ulong pawns, bool color)
+        {
+            float ToReturn = 0.0f;
+
+            foreach (Square s in Enum.GetValues(typeof(Square)))
+            {
+                if (pawns.SquareOccupied(s))
+                {
+                    //Check the rank
+                    //We do not need to handle rank 1 and rank 8 because it is impossible for a pawn to ever be on those ranks
+                    if (s >= Square.A2 && s <= Square.H2)
+                    {
+                        if (color)
+                        {
+                            ToReturn = ToReturn + 0.0f;
+                        }
+                        else
+                        {
+                            ToReturn = ToReturn + 0.5f;
+                        }
+                    }
+                    else if (s >= Square.A3 && s <= Square.H3)
+                    {
+                        if (color)
+                        {
+                            ToReturn = ToReturn + 0.1f;
+                        }
+                        else
+                        {
+                            ToReturn = ToReturn + 0.4f;
+                        }
+                    }
+                    if (s >= Square.A4 && s <= Square.H4)
+                    {
+                        if (color)
+                        {
+                            ToReturn = ToReturn + 0.2f;
+                        }
+                        else
+                        {
+                            ToReturn = ToReturn + 0.3f;
+                        }
+                    }
+                    if (s >= Square.A5 && s <= Square.H5)
+                    {
+                        if (color)
+                        {
+                            ToReturn = ToReturn + 0.3f;
+                        }
+                        else
+                        {
+                            ToReturn = ToReturn + 0.2f;
+                        }
+                    }
+                    if (s >= Square.A6 && s <= Square.H6)
+                    {
+                        if (color)
+                        {
+                            ToReturn = ToReturn + 0.4f;
+                        }
+                        else
+                        {
+                            ToReturn = ToReturn + 0.1f;
+                        }
+                    }
+                    if (s >= Square.A7 && s <= Square.H7)
+                    {
+                        if (color)
+                        {
+                            ToReturn = ToReturn + 0.5f;
+                        }
+                        else
+                        {
+                            ToReturn = ToReturn + 0.0f;
+                        }
+                    }
+                }
+            }
+
+            return ToReturn;
+        }
+        
+        public static float PawnRankingEvaluation(this GameState state)
+        {
+            return PawnRankingEvaluation(state.WhitePawns, true) - PawnRankingEvaluation(state.BlackPawns, false);
         }
 
         #endregion
